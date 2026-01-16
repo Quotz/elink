eed3dfb7d896eb61d14c378541a6a0d8f8021967
andre <andrej.kocevski.koc@gmail.com>
2026-01-16 14:55:24 +0100

    Fix: Use prime256v1 curve for broader OpenSSL compatibility

--- setup-vps.sh
+++ setup-vps.sh
@@ -116,8 +116,8 @@ server {
 
     # TLS Configuration - Compatible with older embedded devices!
     ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ecdh_curve secp384r1:secp256r1:prime256v1;
-    ssl_ciphers 'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-SHA384:DHE-RSA-AES256-GCM-SHA384';
+    ssl_ecdh_curve prime256v1;
+    ssl_ciphers 'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256';
     ssl_prefer_server_ciphers on;
     ssl_session_cache shared:SSL:10m;
     ssl_session_timeout 10m;
